bd74dd69b647582441f224a94f2830cd487338b2
Marian8008 <126537339+Marian8008@users.noreply.github.com>
2023-02-28 15:08:09 -0300

    Add files via upload

---
+++ Proyecto_Final/javafile.js
@@ -0,0 +1,19 @@
+function MostrarLista_2() {
+    var vList = document.getElementById('items_2');
+    if (vList.style.display == "block") { 
+      vList.style.display = "none";
+    }
+     else { 
+      vList.style.display = "block";
+    }
+  }
+
+  function MostrarLista_1() {
+    var vList = document.getElementById('items');
+    if (vList.style.display == "block") { 
+      vList.style.display = "none";
+    }
+     else { 
+      vList.style.display = "block";
+    }
+  }
